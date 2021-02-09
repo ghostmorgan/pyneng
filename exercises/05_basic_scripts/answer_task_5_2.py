4b1035a662e5957dabb57c967816e02264c9600e
Ivan Denisenko <8767180+ghostmorgan@users.noreply.github.com>
2021-02-09 20:01:53 +0300

    Выполнил 5.2

--- exercises/05_basic_scripts/answer_task_5_2.py
+++ exercises/05_basic_scripts/answer_task_5_2.py
@@ -24,24 +24,39 @@ Out[1]: '11111111111111111111111111110000'
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
-template_network = "{0:<10}{1:<10}{2:<10}{3:<10}\n{0:08b}  {1:08b}  {2:08b}  {3:08b}"
-template_mask = "{0:<10}{1:<10}{2:<10}{3:<10}\n{0:08b}  {1:08b}  {2:08b}  {3:08b}"
 
-ip_network = input("Введите IP-сеть в формате: 10.1.1.0/24: ")
-ip_network = ip_network.split('/')
-network = ip_network[0].split('.')
+network = input("Введите адрес сети: ")
 
-mask = "1" * int(ip_network[1]) + "0" * (32 - (int(ip_network[1])))
-list_mask = list()
-list_mask.append(int(mask[:8], 2))
-list_mask.append(int(mask[8:16], 2))
-list_mask.append(int(mask[16:24], 2))
-list_mask.append(int(mask[24:], 2))
+ip, mask = network.split("/")
+ip_list = ip.split(".")
+mask = int(mask)
 
-print("\nNetwork: \n" + template_network.format(
-    int(network[0]), int(network[1]), int(network[2]), int(network[3])
-))
+oct1, oct2, oct3, oct4 = [
+    int(ip_list[0]),
+    int(ip_list[1]),
+    int(ip_list[2]),
+    int(ip_list[3]),
+]
 
-print("\nMask: \n/" + ip_network[1] + "\n" + template_mask.format(
-    int(list_mask[0]), int(list_mask[1]), int(list_mask[2]), int(list_mask[3])
-))
+bin_mask = "1" * mask + "0" * (32 - mask)
+m1, m2, m3, m4 = [
+    int(bin_mask[0:8], 2),
+    int(bin_mask[8:16], 2),
+    int(bin_mask[16:24], 2),
+    int(bin_mask[24:32], 2),
+]
+
+ip_output = """
+Network:
+{0:<8}  {1:<8}  {2:<8}  {3:<8}
+{0:08b}  {1:08b}  {2:08b}  {3:08b}"""
+
+mask_output = """
+Mask:
+/{0}
+{1:<8}  {2:<8}  {3:<8}  {4:<8}
+{1:08b}  {2:08b}  {3:08b}  {4:08b}
+"""
+
+print(ip_output.format(oct1, oct2, oct3, oct4))
+print(mask_output.format(mask, m1, m2, m3, m4))
